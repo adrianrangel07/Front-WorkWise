863856643992b40d27d723fac2bdefbf3c03f668
adrianrangel07 <adrianrangel923@gmail.com>
2025-11-19 00:33:29 -0500

    Redireccionamiento Login & registro, opcion de favoritos + organanizacion de favoritos

--- front-WorkWise/src/app/ofertas/oferta-card/oferta-card.component.ts
+++ front-WorkWise/src/app/ofertas/oferta-card/oferta-card.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { AuthOfertasService } from '../../services/auth-ofertas.service';
 import { AuthPostulacionesService } from '../../services/auth-postulaciones.service';
-import { NgForOf, NgIf } from '@angular/common';
+import { NgForOf, NgIf, NgClass } from '@angular/common';
+// import { NgForOf, NgIf } from '@angular/common';
 import Swal from 'sweetalert2';
 import { S } from '@angular/cdk/keycodes';
 
-
 @Component({
   selector: 'app-oferta-card',
-  imports: [NgForOf, NgIf],
+  imports: [NgForOf, NgIf, NgClass],
   templateUrl: './oferta-card.component.html',
-  styleUrl: './oferta-card.component.css'
+  styleUrl: './oferta-card.component.css',
 })
 export class OfertaCardComponent {
   ofertas: any[] = [];
@@ -23,29 +23,41 @@ export class OfertaCardComponent {
   page = 1;
   pageSize = 8;
   ofertasPaginadas: any[] = [];
+  token: string | null = null;
 
-
-
-  constructor(private authOfertasService: AuthOfertasService, private authPostulacionesService: AuthPostulacionesService) { }
+  constructor(
+    private authOfertasService: AuthOfertasService,
+    private authPostulacionesService: AuthPostulacionesService
+  ) {}
 
   ngOnInit() {
+    // Obtener token
+    this.token = localStorage.getItem('token');
+
+    // Cargar favoritos con token
+    this.cargarFavoritosUsuario();
+
     this.authOfertasService.getOfertas().subscribe({
       next: (data) => {
         this.ofertas = data;
         this.actualizarPaginacion();
         console.log(this.ofertas, this.Tipo_empleo);
-      }, error: (err) => {
+      },
+      error: (err) => {
         console.error('Error al cargar las ofertas', err);
-      }
-    })
+      },
+    });
 
     this.authPostulacionesService.getPostulaciones().subscribe({
       next: (data) => {
-        this.postulaciones = data.map((postulacion: any) => postulacion.oferta.id);
+        this.postulaciones = data.map(
+          (postulacion: any) => postulacion.oferta.id
+        );
         console.log(this.postulaciones);
-      }, error: (err) => {
+      },
+      error: (err) => {
         console.error('Error al cargar las postulaciones', err);
-      }
+      },
     });
   }
 
@@ -65,7 +77,7 @@ export class OfertaCardComponent {
   }
 
   abrirModal(oferta: any) {
-    this.ofertaSeleccionada = oferta
+    this.ofertaSeleccionada = oferta;
     const modal = document.getElementById('modal');
     if (modal) {
       modal.style.display = 'flex';
@@ -82,99 +94,103 @@ export class OfertaCardComponent {
     this.ofertaSeleccionada = null;
   }
 
-  normalizarDatos(ofertaData: any){
+  normalizarDatos(ofertaData: any) {
     switch (ofertaData.experiencia) {
-        case 0:
-          this.experiencia = "Sin experiencia";
-          break
-        case 1:
-          this.experiencia = "Menos de 1 año";
-          break
-        case 2:
-          this.experiencia = "Entre 1 y 3 años";
-          break
-        case 3:
-          this.experiencia = "Entre 3 y 5 años";
-          break
-        case 4:
-          this.experiencia = "Entre 5 y 10 años";
-          break
-        case 5:
-          this.experiencia = "Más de 10 años";
-          break
-        default:
-          this.experiencia = "No especificado";
-      }
-      switch (ofertaData.tipo_Empleo) {
-        case 'Tiempo_Completo':
-          this.Tipo_empleo = "Tiempo Completo";
-          break
-        case "Medio_Tiempo":
-          this.Tipo_empleo = "Medio Tiempo";
-          break
-        case "Por_Horas":
-          this.Tipo_empleo = "Por Horas";
-          break
-        case "Freelance":
-          this.Tipo_empleo = "Freelance";
-          break
-        default:
-          this.Tipo_empleo = "No especificado";
-      }
-      switch (ofertaData.tipo_Contrato) {
-        case "Obra_Labor":
-          this.tipo_Contrato = "Obra labor";
-          break
-        case "Fijo":
-          this.tipo_Contrato = "Fijo";
-          break
-        case "Indefinido":
-          this.tipo_Contrato = "Indefinido";
-          break
-        case "Practicas":
-          this.tipo_Contrato = "Practicas";
-          break
-        default:
-          this.tipo_Contrato = "No especificado";
-      }
-      switch (ofertaData.nivel_Educacion) {
-        case "Sin_estudios":
-          this.nivel_Educacion = "Sin estudios";
-          break
-        case "Bachiller":
-          this.nivel_Educacion = "Bachiller";
-          break
-        case "Tecnico_Tecnologo":
-          this.nivel_Educacion = "Tecnico/Tecnologo";
-          break
-        case "Tecnico_Universitario":
-          this.nivel_Educacion = "Tecnico o Universitario";
-          break
-        case "Universitario":
-          this.nivel_Educacion = "Universitario";
-          break
-        case "Master":
-          this.nivel_Educacion = "Master";
-          break
-        case "Doctorado":
-          this.nivel_Educacion = "Doctorado";
-          break
-        default:
-          this.nivel_Educacion = "No especificado";
-      }
-  }
-
-  getTipoEmpleo(valor: string) {
-    console.log(valor)
-    switch (valor) {
-      case 'Tiempo_Completo': return 'Tiempo Completo';
-      case 'Medio_Tiempo': return 'Medio Tiempo';
-      case 'Por_Horas': return 'Por Horas';
-      case 'Freelance': return 'Freelance';
-      default: return 'No especificado';
+      case 0:
+        this.experiencia = 'Sin experiencia';
+        break;
+      case 1:
+        this.experiencia = 'Menos de 1 año';
+        break;
+      case 2:
+        this.experiencia = 'Entre 1 y 3 años';
+        break;
+      case 3:
+        this.experiencia = 'Entre 3 y 5 años';
+        break;
+      case 4:
+        this.experiencia = 'Entre 5 y 10 años';
+        break;
+      case 5:
+        this.experiencia = 'Más de 10 años';
+        break;
+      default:
+        this.experiencia = 'No especificado';
+    }
+    switch (ofertaData.tipo_Empleo) {
+      case 'Tiempo_Completo':
+        this.Tipo_empleo = 'Tiempo Completo';
+        break;
+      case 'Medio_Tiempo':
+        this.Tipo_empleo = 'Medio Tiempo';
+        break;
+      case 'Por_Horas':
+        this.Tipo_empleo = 'Por Horas';
+        break;
+      case 'Freelance':
+        this.Tipo_empleo = 'Freelance';
+        break;
+      default:
+        this.Tipo_empleo = 'No especificado';
+    }
+    switch (ofertaData.tipo_Contrato) {
+      case 'Obra_Labor':
+        this.tipo_Contrato = 'Obra labor';
+        break;
+      case 'Fijo':
+        this.tipo_Contrato = 'Fijo';
+        break;
+      case 'Indefinido':
+        this.tipo_Contrato = 'Indefinido';
+        break;
+      case 'Practicas':
+        this.tipo_Contrato = 'Practicas';
+        break;
+      default:
+        this.tipo_Contrato = 'No especificado';
+    }
+    switch (ofertaData.nivel_Educacion) {
+      case 'Sin_estudios':
+        this.nivel_Educacion = 'Sin estudios';
+        break;
+      case 'Bachiller':
+        this.nivel_Educacion = 'Bachiller';
+        break;
+      case 'Tecnico_Tecnologo':
+        this.nivel_Educacion = 'Tecnico/Tecnologo';
+        break;
+      case 'Tecnico_Universitario':
+        this.nivel_Educacion = 'Tecnico o Universitario';
+        break;
+      case 'Universitario':
+        this.nivel_Educacion = 'Universitario';
+        break;
+      case 'Master':
+        this.nivel_Educacion = 'Master';
+        break;
+      case 'Doctorado':
+        this.nivel_Educacion = 'Doctorado';
+        break;
+      default:
+        this.nivel_Educacion = 'No especificado';
     }
   }
 
+  getTipoEmpleo(valor: string) {
+    console.log(valor);
+    switch (valor) {
+      case 'Tiempo_Completo':
+        return 'Tiempo Completo';
+      case 'Medio_Tiempo':
+        return 'Medio Tiempo';
+      case 'Por_Horas':
+        return 'Por Horas';
+      case 'Freelance':
+        return 'Freelance';
+      default:
+        return 'No especificado';
+    }
+  }
 
   postularse(ofertaId: number) {
     this.authPostulacionesService.postularse(ofertaId).subscribe({
@@ -184,7 +200,7 @@ export class OfertaCardComponent {
             title: 'Inicia sesión',
             text: 'Debes iniciar sesión para postularte a una oferta.',
             icon: 'warning',
-            confirmButtonText: 'Aceptar'
+            confirmButtonText: 'Aceptar',
           });
           return;
         }
@@ -194,8 +210,8 @@ export class OfertaCardComponent {
             text: response.message,
             icon: 'success',
             timer: 2000,
-            showConfirmButton: false
-          })
+            showConfirmButton: false,
+          });
           this.postulaciones.push(ofertaId);
         } else {
           Swal.fire({
@@ -203,17 +219,84 @@ export class OfertaCardComponent {
             text: response.message,
             icon: 'info',
             timer: 2000,
-            showConfirmButton: false
+            showConfirmButton: false,
           });
         }
-      }, error: (err) => {
+      },
+      error: (err) => {
         console.error('Error al postularse a la oferta:', err);
         Swal.fire({
           title: 'Error',
           text: 'No se pudo completar la postulación. Inténtelo de nuevo.',
-          icon: 'error'
+          icon: 'error',
         });
-      }
+      },
     });
+  }
+
+  favoritos: number[] = [];
+  esFavorito(id: number): boolean {
+    return this.favoritos.includes(id);
+  }
+
+  toggleFavorito(oferta: any, event: MouseEvent) {
+    event.stopPropagation();
+
+    // Si NO hay token → no permitir
+    if (!this.token) {
+      Swal.fire(
+        'Inicia sesión',
+        'Debes iniciar sesión para guardar favoritos.',
+        'warning'
+      );
+      return;
+    }
+
+    // Si ya es favorito → quitar
+    if (this.esFavorito(oferta.id)) {
+      this.favoritos = this.favoritos.filter((id) => id !== oferta.id);
+    }
+    // Si no es → agregar
+    else {
+      this.favoritos.push(oferta.id);
+    }
+
+    // Guardar favoritos ligados al token
+    localStorage.setItem(
+      'favoritos_' + this.token,
+      JSON.stringify(this.favoritos)
+    );
+
+    // Reordenar si tu toggle está activado
+    this.ordenarOfertas();
+    this.actualizarPaginacion();
+  }
+
+  mostrarFavoritosPrimero: boolean = false;
+
+  ordenarFavoritosDesdePadre() {
+    this.mostrarFavoritosPrimero = !this.mostrarFavoritosPrimero;
+    this.ordenarOfertas();
+    this.actualizarPaginacion();
+  }
+
+  ordenarOfertas() {
+    if (!this.mostrarFavoritosPrimero) return;
+
+    this.ofertas.sort((a, b) => {
+      const aFav = this.esFavorito(a.id) ? 1 : 0;
+      const bFav = this.esFavorito(b.id) ? 1 : 0;
+      return bFav - aFav;
+    });
+  }
+
+  cargarFavoritosUsuario() {
+    if (!this.token) {
+      this.favoritos = []; // No logueado → no favoritos
+      return;
+    }
+
+    const data = localStorage.getItem('favoritos_' + this.token);
+    this.favoritos = data ? JSON.parse(data) : [];
   }
 }
--- front-WorkWise/src/app/ofertas/ofertas.component.ts
+++ front-WorkWise/src/app/ofertas/ofertas.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
-import { NavbarAcordionComponent } from "../reutilzar/navbar-acordion/navbar-acordion.component";
-import { NavbarbusquedaComponent } from "../reutilzar/navbarbusqueda/navbarbusqueda.component";
+import { Component, ViewChild } from '@angular/core';
+import { NavbarAcordionComponent } from '../reutilzar/navbar-acordion/navbar-acordion.component';
+import { NavbarbusquedaComponent } from '../reutilzar/navbarbusqueda/navbarbusqueda.component';
 import { AuthPersonaService } from '../services/auth-personsa.service';
 import { OfertaCardComponent } from './oferta-card/oferta-card.component';
-import { FooterComponent } from "../reutilzar/footer/footer.component";
+import { FooterComponent } from '../reutilzar/footer/footer.component';
 
 @Component({
   selector: 'app-ofertas',
-  imports: [NavbarAcordionComponent, NavbarbusquedaComponent, OfertaCardComponent, FooterComponent],
+  imports: [
+    NavbarAcordionComponent,
+    NavbarbusquedaComponent,
+    OfertaCardComponent,
+    FooterComponent,
+  ],
   templateUrl: './ofertas.component.html',
-  styleUrl: './ofertas.component.css'
+  styleUrl: './ofertas.component.css',
 })
 export class OfertasComponent {
-    ofertas: any[] = [];
-    logueado = false;
+  ofertas: any[] = [];
+  logueado = false;
 
   constructor(private authService: AuthPersonaService) {}
+  @ViewChild(OfertaCardComponent)
+  ofertaCard!: OfertaCardComponent;
+
+  mostrarFavoritosPrimero: boolean = false;
+
+  toggleMostrarFavoritos() {
+    this.mostrarFavoritosPrimero = !this.mostrarFavoritosPrimero;
+    this.ofertaCard.ordenarFavoritosDesdePadre(); 
+  }
 }
