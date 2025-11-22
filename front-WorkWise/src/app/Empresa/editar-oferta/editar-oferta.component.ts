a592944c0ee55136fc12668cdfbdd9358a1f3e2c
yonierva <yoniervasquez98@gmail.com>
2025-11-22 04:07:00 -0500

    editar ofertas

--- front-WorkWise/src/app/Empresa/editar-oferta/editar-oferta.component.ts
+++ front-WorkWise/src/app/Empresa/editar-oferta/editar-oferta.component.ts
@@ -1,11 +1,168 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthOfertasService } from '../../services/auth-ofertas.service';
+import { Router } from '@angular/router';
+import { NavbarAcordionComponent } from "../../reutilzar/navbar-acordion/navbar-acordion.component";
+import { NavbarbusquedaComponent } from "../../reutilzar/navbarbusqueda/navbarbusqueda.component";
+import { FooterComponent } from "../../reutilzar/footer/footer.component";
+import Swal from 'sweetalert2';
+import flatpickr from "flatpickr";
+import { Spanish } from 'flatpickr/dist/l10n/es.js';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-editar-oferta',
-  imports: [],
+  imports: [NavbarAcordionComponent, NavbarbusquedaComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './editar-oferta.component.html',
   styleUrl: './editar-oferta.component.css'
 })
-export class EditarOfertaComponent {
+export class EditarOfertaComponent implements OnInit {
+  oferta: any = {};
+  step: number = 1
+  habilidades: string[] = [];
+  nuevaHabilidad: string = '';
+  showPreview: boolean = false;
+  showDropdown: boolean = false;
+  selectedValue = '';
+  selectedLabel = '';
+
+  options = [
+    { value: 'Aeroespacial', label: 'Aeroespacial' },
+    { value: 'Agricultura', label: 'Agricultura' },
+    { value: 'Agroindustria', label: 'Agroindustria' },
+    { value: 'Ambiental', label: 'Medio ambiente' },
+    { value: 'ArtesCreativas', label: 'Artes creativas' },
+    { value: 'Automotriz', label: 'Automotriz' },
+    { value: 'BienesRaices', label: 'Bienes raíces' },
+    { value: 'Biotecnologia', label: 'Biotecnología' },
+    { value: 'Comercio', label: 'Comercio' },
+    { value: 'Construccion', label: 'Construcción' },
+    { value: 'Consultoria', label: 'Consultoría' },
+    { value: 'Deportes', label: 'Deportes y recreación' },
+    { value: 'Diseño', label: 'Diseño' },
+    { value: 'Educacion', label: 'Educación' },
+    { value: 'Energia', label: 'Energía' },
+    { value: 'Finanzas', label: 'Finanzas' },
+    { value: 'Gobierno', label: 'Gobierno' },
+    { value: 'Investigacion', label: 'Investigación' },
+    { value: 'Legal', label: 'Legal' },
+    { value: 'Logistica', label: 'Logística' },
+    { value: 'Manufactura', label: 'Manufactura' },
+    { value: 'Marketing', label: 'Marketing' },
+    { value: 'Medios', label: 'Medios de comunicación' },
+    { value: 'Mineria', label: 'Minería' },
+    { value: 'Naval', label: 'Naval' },
+    { value: 'Quimica', label: 'Química y farmacéutica' },
+    { value: 'RRHH', label: 'Recursos humanos' },
+    { value: 'Salud', label: 'Salud' },
+    { value: 'Seguros', label: 'Seguros' },
+    { value: 'Servicios', label: 'Servicios' },
+    { value: 'Social', label: 'Trabajo social' },
+    { value: 'Tecnologia', label: 'Tecnología' },
+    { value: 'Textil', label: 'Textil y confección' },
+    { value: 'Transporte', label: 'Transporte' },
+    { value: 'Turismo', label: 'Turismo' }
+  ];
+
+  filteredOptions = [...this.options];
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  filterOptions(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.filteredOptions = this.options.filter(opt =>
+      opt.label.toLowerCase().includes(query)
+    );
+  }
+
+  selectOption(item: any) {
+    this.oferta.sector_oferta = item.value
+    this.selectedValue = item.value;
+    this.selectedLabel = item.label;
+    this.showDropdown = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.col-sm-6 mb-3')) {
+      this.showDropdown = false;
+    }
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
+  addHabilidad() {
+    const valor = this.nuevaHabilidad.trim();
+    console.log(valor)
+
+    if (!valor) return;
+    this.habilidades.push(valor);
+
+    this.nuevaHabilidad = '';
+  }
+
+  removeHabilidad(index: number) {
+    this.habilidades.splice(index, 1);
+  }
+
+  constructor(private authOfertasService: AuthOfertasService, private router: Router) { }
+
+  ngOnInit() {
+    this.oferta = this.authOfertasService.getOfertaSeleccionada();
+    console.log(this.oferta);
+  }
+
+  guardarCambios() {
+    this.oferta.habilidades = this.habilidades.map(h => ({habilidad: { nombre: h }}));
+
+    this.authOfertasService.actualizarOferta(this.oferta.id, this.oferta)
+      .subscribe({
+        next: (res) => {
+          console.log("Oferta actualizada", res);
+          Swal.fire({
+            icon: 'success',
+            title: 'Oferta actualizada',
+            timer: 2000
+          });
+        },
+        error: (err) => {
+          console.error("Error al actualizar: ", err);
+        }
+      });
+  }
+
+  nextStep() {
+    if (this.step < 3) {
+      this.step++;
+      console.log();
+      setTimeout(() => {
+        const fechaInput = document.getElementById('fecha');
+
+        if (fechaInput) {
+          flatpickr(fechaInput, {
+            dateFormat: 'Y-m-d',
+            altFormat: 'd/m/Y',
+            locale: Spanish,
+            minDate: 'today',
+            maxDate: ''
+          });
+        }
+        console.log(fechaInput);
+      }, 100);
+    }
+  }
+
+  prevStep() {
+    if (this.step > 1) {
+      this.step--;
+    }
+  }
+
 
 }
--- front-WorkWise/src/app/services/auth-ofertas.service.ts
+++ front-WorkWise/src/app/services/auth-ofertas.service.ts
@@ -2,31 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 
-interface Oferta {
-  id: number;
-  titulo: string;
-  empresa: {
-    id: number;
-    nombre: string;
-  };
-  salario: number;
-  moneda: string;
-  ubicacion: string;
-  tipo_Empleo: string;
-  descripcion: string;
-  habilidades: string[];
-  nivel_Educacion: string;
-  experiencia: number;
-  modalidad: string;
-  sector_oferta: string;
-  fecha_Cierre: Date;
-  activo: boolean;
-}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthOfertasService {
-  private ofertaSeleccionada: Oferta | null = null;
+  private ofertaSeleccionada: any | null = null;
 
   private apiUrl = 'http://localhost:8080/api/ofertas';
 
@@ -37,30 +18,30 @@ export class AuthOfertasService {
     return this.http.get<any[]>(`${this.apiUrl}/home`);
   }
 
-  actualizarOferta(id: number, oferta: Oferta): Observable<Oferta> {
+  actualizarOferta(id: number, oferta: any): Observable<any> {
     const token = localStorage.getItem('token');
 
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
-    return this.http.put<Oferta>(`${this.apiUrl}/${id}`, oferta, { headers });
+    return this.http.put<any>(`${this.apiUrl}/${id}`, oferta, { headers });
   }
 
-  toggleOferta(id: number): Observable<Oferta> {
+  toggleOferta(id: number): Observable<any> {
     const token = localStorage.getItem('token');
 
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
 
-    return this.http.put<Oferta>(
+    return this.http.put<any>(
       `${this.apiUrl}/${id}/toggle`,
       {},
       { headers }
     );
   }
 
-  setOfertaSeleccionada(oferta: Oferta) {
+  setOfertaSeleccionada(oferta: any) {
     this.ofertaSeleccionada = oferta;
   }
 
-  getOfertaSeleccionada(): Oferta | null {
+  getOfertaSeleccionada(): any | null {
     return this.ofertaSeleccionada;
   }
 
