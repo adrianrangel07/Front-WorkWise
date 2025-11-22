c8add2d0429128e27326e256dd76f5186687faab
adrianrangel07 <adrianrangel923@gmail.com>
2025-11-22 08:58:27 -0500

    Ofertas carga

--- front-WorkWise/src/app/ofertas/ofertas.component.ts
+++ front-WorkWise/src/app/ofertas/ofertas.component.ts
@@ -5,6 +5,7 @@ import { AuthPersonaService } from '../services/auth-personsa.service';
 import { OfertaCardComponent } from './oferta-card/oferta-card.component';
 import { FooterComponent } from '../reutilzar/footer/footer.component';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ofertas',
@@ -13,6 +14,7 @@ import { ActivatedRoute } from '@angular/router';
     NavbarbusquedaComponent,
     OfertaCardComponent,
     FooterComponent,
+    CommonModule,
   ],
   templateUrl: './ofertas.component.html',
   styleUrl: './ofertas.component.css',
@@ -26,21 +28,32 @@ export class OfertasComponent {
     private authService: AuthPersonaService,
     private route: ActivatedRoute
   ) {}
-  
+
   @ViewChild(OfertaCardComponent)
   ofertaCard!: OfertaCardComponent;
 
   mostrarFavoritosPrimero: boolean = false;
+  loading: boolean = true;
 
   ngOnInit() {
+    this.loading = true;
+    setTimeout(() => {
+      if (this.ofertaCard) {
+        if (this.terminoBusqueda) {
+          this.ofertaCard.filtrarOfertas(this.terminoBusqueda);
+        }
+        this.loading = false;
+      }
+    }, 800);
+
     // Escuchar los parámetros de la URL
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       if (params['termino']) {
         this.terminoBusqueda = params['termino'];
         // Si el componente ofertaCard ya está cargado, aplicar el filtro
         setTimeout(() => {
           if (this.ofertaCard) {
-            this.ofertaCard.filtrarOfertas(this.terminoBusqueda); 
+            this.ofertaCard.filtrarOfertas(this.terminoBusqueda);
           }
         });
       }
@@ -49,7 +62,7 @@ export class OfertasComponent {
 
   toggleMostrarFavoritos() {
     this.mostrarFavoritosPrimero = !this.mostrarFavoritosPrimero;
-    this.ofertaCard.ordenarFavoritosDesdePadre(); 
+    this.ofertaCard.ordenarFavoritosDesdePadre();
   }
 
   onBuscar(termino: string) {
@@ -60,6 +73,7 @@ export class OfertasComponent {
   }
 
   onAplicarFiltros(filtros: any) {
+    this.loading = true;
     if (this.ofertaCard) {
       this.ofertaCard.aplicarFiltrosAvanzados(filtros);
     }
